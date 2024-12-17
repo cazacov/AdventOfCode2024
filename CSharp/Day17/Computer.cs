bf2c1769e1602608efe422feea137c37e59afa8c
Victor Kazakov <cazacov.git@gmail.com>
2024-12-17 13:46:23 +0100

    Day 17 puzzles solved

---
+++ CSharp/Day17/Computer.cs
@@ -0,0 +1,139 @@
+﻿namespace Day17
+{
+    internal class Computer
+    {
+        public long RegisterA;
+        public long RegisterB;
+        public long RegisterC;
+        public int InstructionPointer;
+
+        public List<int> Instructions = new List<int>();
+        public List<int> Output = new List<int>();
+
+        public enum Instruction
+        {
+            ADV = 0,
+            BXL = 1,
+            BST = 2,
+            JNZ = 3,
+            BXC = 4,
+            OUT = 5,
+            BDV = 6,
+            CDV = 7
+        }
+
+        public void RunToEnd()
+        {
+            while (InstructionPointer < Instructions.Count - 1)
+            {
+                RunOneStep();
+            }
+        }
+
+        public int RunToFirstOut()
+        {
+            while (InstructionPointer < Instructions.Count - 1)
+            {
+                RunOneStep();
+                if (Output.Count > 0)
+                {
+                    return Output[0];
+                }
+            }
+            return -1;
+        }
+
+
+        private void RunOneStep()
+        {
+            var opcode = Instructions[InstructionPointer];
+            var instruction = (Instruction)opcode;
+            long operand = Instructions[InstructionPointer+1];
+            InstructionPointer += 2;
+
+            if (!IsLiteral(instruction))
+            {
+                operand = ExpandCombo(operand);
+            }
+            ExecuteCommand(instruction, operand);
+        }
+
+        private void ExecuteCommand(Instruction instruction, long operand)
+        {
+            switch (instruction)
+            {
+                case Instruction.ADV:
+                    RegisterA >>= (int)operand;
+                    break;
+                case Instruction.BXL:
+                    RegisterB ^= operand;
+                    break;
+                case Instruction.BST:
+                    RegisterB = operand & 0x00000007;
+                    break;
+                case Instruction.JNZ:
+                    if (RegisterA != 0)
+                    {
+                        InstructionPointer = (int)operand;
+                        if (InstructionPointer < 0 || InstructionPointer >= Instructions.Count - 1)
+                        {
+                            throw new Exception("Invalid jump to non-existing address " + operand);
+                        }
+                    }
+                    break;
+                case Instruction.BXC:
+                    RegisterB ^= RegisterC;
+                    break;
+                case Instruction.OUT:
+                    Output.Add((int)(operand & 0x00000007));
+                    break;
+                case Instruction.BDV:
+                    RegisterB = RegisterA >> (int)operand;
+                    break;
+                case Instruction.CDV:
+                    RegisterC = RegisterA >> (int)operand;
+                    break;
+            }
+        }
+
+        private long ExpandCombo(long operand)
+        {
+            if (operand < 4)
+            {
+                return operand;
+            }
+            if (operand == 4)
+            {
+                return RegisterA;
+            }
+            if (operand == 5)
+            {
+                return RegisterB;
+            }
+            if (operand == 6)
+            {
+                return RegisterC;
+            }
+            if (operand == 7)
+            {
+                throw  new Exception("Invalid operand");
+            }
+            return operand;
+        }
+
+        private bool IsLiteral(Instruction instruction)
+        {
+            return instruction == Instruction.BXL
+                   || instruction == Instruction.JNZ;
+        }
+
+        public void Reset(long registerA)
+        {
+            RegisterA = registerA;
+            RegisterB = 0;
+            RegisterC = 0;
+            InstructionPointer = 0;
+            Output.Clear();
+        }
+    }
+}
---
+++ CSharp/Day17/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace Day17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = ReadInput();
+            Console.WriteLine($"Day 17 Part 1: {Part1(input)}");
+            Console.WriteLine("\n\n\n");
+            Console.WriteLine($"Day 17 Part 2: {Part2(input)}");
+        }
+
+        private static string Part1(Computer computer)
+        {
+            computer.RunToEnd();
+            return string.Join(",", computer.Output);
+        }
+
+        private static string Part2(Computer computer)
+        {
+            var areg = FindRecursive(computer, 0L, computer.Instructions.Count - 1);
+           
+            computer.Reset(areg);
+            computer.RunToEnd();
+
+            // Confirm the output is correct
+            if (computer.Output.Count == computer.Instructions.Count)
+            {
+                for (var i = 0; i < computer.Output.Count; i++)
+                {
+                    if (computer.Output[i] != computer.Instructions[i])
+                    {
+                        throw new Exception("Part 2 has errors");
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("Part 2 has errors");
+            }
+                
+            return areg.ToString();
+        }
+
+        private static long FindRecursive(Computer computer, long leftPart, int level)
+        {
+            if (level < 0)
+            {
+                return leftPart;
+            }
+            
+            var expectedOutput = computer.Instructions[level];
+            for (var digit = 0; digit < 8; digit++)
+            {
+                var candidateInput = leftPart * 8L + digit;
+                computer.Reset(candidateInput);
+                var output = computer.RunToFirstOut();
+                if (output == expectedOutput)
+                {
+                    var result = FindRecursive(computer, leftPart * 8 + digit, level - 1);
+                    if (result > 0)
+                    {
+                        return result;
+                    }
+                }
+            }
+            // That should never happen
+            return -1;
+        }
+
+        private static Computer ReadInput()
+        {
+            var lines = File.ReadAllLines("input.txt");
+
+            var computer = new Computer
+            {
+                RegisterA = long.Parse(lines[0][12..]),
+                RegisterB = long.Parse(lines[1][12..]),
+                RegisterC = long.Parse(lines[2][12..])
+            };
+
+            var program = lines[4][9..];
+            computer.Instructions = program.Split(",").Select(int.Parse).ToList();
+            return computer;
+        }
+    }
+}
