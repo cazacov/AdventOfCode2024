a5cbc5a9f23f1d8d57c908c763c93928f551a9cb
Victor Kazakov <cazacov.git@gmail.com>
2024-12-18 06:49:54 +0100

    Day 18 puzzles solved

---
+++ CSharp/Day18/Pos.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day18
+{
+    internal class Pos
+    {
+        public Pos(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        protected bool Equals(Pos other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((Pos)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y);
+        }
+
+        public int X;
+        public int Y;
+        public int Cost;
+
+        public IEnumerable<Pos> Neigbors(HashSet<Pos> free, int width, int height)
+        {
+            for (int dx = -1; dx <= 1; dx++)
+            {
+                for (int dy = -1; dy <= 1; dy++)
+                {
+                    if (dx == 0 && dy == 0) continue;
+                    if (dx != 0 && dy != 0) continue;
+                    var x = X + dx;
+                    var y = Y + dy;
+                    if (x < 0 || x >= width || y < 0 || y >= height) continue;
+                    var pos = free.FirstOrDefault(f => f.X == x && f.Y == y);
+                    if (pos != null)
+                    {
+                        yield return pos;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CSharp/Day18/Program.cs
@@ -0,0 +1,165 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace Day18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = ReadInput();
+            Console.WriteLine($"Day 18 Part 1: {Part1(input)}");
+            Console.WriteLine("\n\n\n");
+            Console.WriteLine($"Day 18 Part 2: {Part2(input)}");
+        }
+
+        private static string Part1(List<Pos> bytes)
+        {
+            var width = 71;
+            var height = 71;
+
+            var corrupted = new HashSet<Pos>();
+            corrupted.UnionWith(bytes.Take(1024));
+
+            var free = new HashSet<Pos>();
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++)
+                {
+                    var pos = new Pos(x, y);
+                    pos.Cost = int.MaxValue;
+                    if (!corrupted.Contains(pos))
+                    {
+                        free.Add(pos);
+                    }
+                }
+            }
+
+            var start = free.FirstOrDefault(f => f.X == 0 && f.Y == 0);
+            var finish = free.FirstOrDefault(f => f.X == width - 1 && f.Y == height - 1);
+            start.Cost = 0;
+
+
+            var visited = new HashSet<Pos>();
+            var queue = new HashSet<Pos>();
+            queue.Add(start);
+
+            while (true)
+            {
+                var current = queue.OrderBy(q => q.Cost).First();
+                visited.Add(current);
+                queue.Remove(current);
+
+                if ( current.Equals(finish))
+                {
+                    break;
+                }
+
+                foreach (var neighbor in current.Neigbors(free, width, height)) { 
+                    if (visited.Contains(neighbor)) continue;
+                    var newCost = current.Cost + 1;
+                    if (newCost < neighbor.Cost)
+                    {
+                        neighbor.Cost = newCost;
+                    }
+                    queue.Add(neighbor);
+                }
+
+            }
+            return finish.Cost.ToString();
+
+        }
+
+        private static string Part2(List<Pos> bytes)
+        {
+            var width = 71;
+            var height = 71;
+            int startCount = 1024;
+
+            var corrupted = new HashSet<Pos>();
+            corrupted.UnionWith(bytes.Take(startCount));
+
+            var free = new HashSet<Pos>();
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    var pos = new Pos(x, y);
+                    pos.Cost = int.MaxValue;
+                    if (!corrupted.Contains(pos))
+                    {
+                        free.Add(pos);
+                    }
+                }
+            }
+            var start = free.FirstOrDefault(f => f.X == 0 && f.Y == 0);
+            var finish = free.FirstOrDefault(f => f.X == width - 1 && f.Y == height - 1);
+            start.Cost = 0;
+
+            var index = startCount;
+            while (index < bytes.Count)
+            {
+                var next = bytes[index];
+                free.RemoveWhere(f => f.X == next.X && f.Y == next.Y);
+                Console.WriteLine(free.Count);
+                Reset(free);
+                if (!PathExists(free, start, finish, width, height))
+                {
+                    return $"{next.X},{next.Y}";
+                }
+                index++;
+            }
+            return "ERROR";
+        }
+
+        private static bool PathExists(HashSet<Pos> free, Pos? start, Pos? finish, int width, int height)
+        {
+            var visited = new HashSet<Pos>();
+            var queue = new HashSet<Pos>();
+            queue.Add(start);
+
+            while (true)
+            {
+                if (queue.Count == 0) return false;
+                var current = queue.First();
+
+                visited.Add(current);
+                queue.Remove(current);
+
+                if (current.Equals(finish))
+                {
+                    return true;
+                }
+
+                foreach (var neighbor in current.Neigbors(free, width, height))
+                {
+                    if (visited.Contains(neighbor)) continue;
+                    if (queue.Contains(neighbor)) continue;
+                    queue.Add(neighbor);
+                }
+            }
+            return true;
+        }
+
+        private static void Reset(HashSet<Pos> free)
+        {
+            foreach (var pos in free)
+            {
+                if (pos.X != 0 && pos.Y != 0)
+                {
+                    pos.Cost = int.MaxValue;
+                }
+            }
+        }
+
+        private static List<Pos> ReadInput()
+        {
+            var result = new List<Pos>();
+            var lines = File.ReadAllLines("input.txt");
+            foreach (var line in lines)
+            {
+                var parts = line.Split(',');
+                result.Add(new Pos(int.Parse(parts[0]), int.Parse(parts[1])));
+            }
+            return result;
+        }
+    }
+}
