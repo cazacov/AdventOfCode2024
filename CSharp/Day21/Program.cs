39deb5afc0ebfb1e33732d4e47955c3cd3c04d76
Victor Kazakov <cazacov.git@gmail.com>
2024-12-25 14:12:06 +0100

    Day 25 puzzle solved

---
+++ CSharp/Day21/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Day21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = ReadInput();
+            Console.WriteLine($"Day 21 Part 1: {Part1(input)}");
+            Console.WriteLine("\n\n\n");
+            Console.WriteLine($"Day 21 Part 2: {Part2(input)}");
+        }
+
+        private static string Part1(List<string> input)
+        {
+            return "not implemented";
+        }
+
+        private static string Part2(List<string> input)
+        {
+            return "not implemented";
+        }
+
+        private static List<string> ReadInput()
+        {
+            var lines = File.ReadAllLines("input.txt");
+            return lines.ToList();
+        }
+    }
+}
---
+++ CSharp/Day25/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace Day25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = ReadInput();
+            var locks= input.Item1;
+            var keys = input.Item2;
+            Console.WriteLine($"Day 25 Part 1: {Part1(locks, keys)}");
+            Console.WriteLine("\n\n\n");
+            Console.WriteLine($"Day 25 Part 2: {Part2(locks, keys)}");
+        }
+
+        private static string Part1(List<List<int>> locks, List<List<int>> keys)
+        {
+            int matches = 0;
+
+            foreach (var llock in locks)
+            {
+                foreach (var key in keys)
+                {
+                    if (DoMatch(llock, key))
+                    {
+                        matches++;
+                    }
+                }
+            }
+            return matches.ToString();
+        }
+
+        private static bool DoMatch(List<int> llock, List<int> key)
+        {
+            for (int i = 0; i < llock.Count; i++)
+            {
+                if (llock[i] + key[i] > 5)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static string Part2(List<List<int>> locks, List<List<int>> keys)
+        {
+            return "";
+        }
+
+        private static Tuple<List<List<int>>, List<List<int>>> ReadInput()
+        {
+            var locks = new List<List<int>>();
+            var keys = new List<List<int>>();
+
+            void processBlock(List<string> block)
+            {
+                var key = new List<int>();
+                for (var x = 0; x < block[0].Length; x++)
+                {
+                    key.Add(-1);
+                    for (var y = 0; y < block.Count; y++)
+                    {
+                        if (block[y][x] == '#')
+                        {
+                            key[x] += 1;
+                        }
+                    }
+                }
+
+                if (block[0].StartsWith("#"))
+                {
+                    locks.Add(key);
+                }
+                else
+                {
+                    keys.Add(key);
+                }
+            }
+
+            var lines = File.ReadAllLines("input.txt");
+            var current = new List<string>();
+            foreach (var line in lines)
+            {
+                if (String.IsNullOrWhiteSpace(line))
+                {
+                    processBlock(current);
+                    current.Clear();
+                }
+                else
+                {
+                    current.Add(line);
+                }
+            }
+            processBlock(current);
+            return new Tuple<List<List<int>>, List<List<int>>>(locks, keys);
+        }
+    }
+}
